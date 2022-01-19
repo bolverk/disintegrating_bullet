f001f03b6350de2267d9a4392235ccccadd07b30
Almog Yalinewich <almog.yalin@gmail.com>
2022-01-18 21:57:24 -0500

    add eruption criterion

--- disintegrating_plug/lgrb.py
+++ disintegrating_plug/lgrb.py
@@ -1,11 +1,21 @@
 import sympy
 from probe import show
+from equation_of_motion import alpha
+from rhd import c
+
+sympy.var('x rho_a', positive=True)
 
 def calc_hydrostatic_density():
 
-    sympy.var('M_e R_e x omega', positive=True)
+    sympy.var('M_e R_e omega', positive=True)
 
     return (M_e/R_e**3)*(x/R_e)**omega
+
+def eruption_criterion():
+
+    sympy.var('L')
+
+    return L/(alpha**2*x**2*c) - rho_a*c**2/alpha**4
 
 if __name__ == '__main__':
 
